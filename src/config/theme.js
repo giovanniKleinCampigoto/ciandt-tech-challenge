94346187b781d41f37535a207c10eab1f73bb9e2
David <david.vvmm1997@gmail.com>
2022-07-26 11:43:40 -0500

    Initial stup

---
+++ src/config/theme.js
@@ -0,0 +1,5 @@
+export const lightBlue = "#8ecae6";
+export const seaBlue = "#219ebc";
+export const darkBlue = "#023047";
+export const yellow = "#ffb703";
+export const orange = "#fb8500";
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+import App from "components/App";
+import * as theme from "config/theme";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+
+  body {
+    background-color: ${ (props) => props.theme.lightBlue };
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={ theme }>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
