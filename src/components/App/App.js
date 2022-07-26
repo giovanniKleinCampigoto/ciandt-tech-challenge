227cd88e71dac62fd67b589c978e8d6bd0e53b12
David <david.vvmm1997@gmail.com>
2022-07-26 17:01:21 -0500

    feat: basic router setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import Navbar from "./Navbar";
 import PokemonGrid from "./PokemonGrid";
 import Searchbar from "./Searchbar";
+import PokemonDetails from "pages/PokemonDetails";
 
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      <Searchbar />
-      <PokemonGrid />
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Searchbar />
+              <PokemonGrid />
+            </>
+          }
+          />
+          <Route path="/description" element={<PokemonDetails />}
+          />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/PokemonDetails/PokemonDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokemonDetails = () => {
+  return (
+    <div>
+      Details
+    </div>
+  );
+}
+
+export default PokemonDetails;
